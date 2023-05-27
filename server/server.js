69f9f44f409935c63eed8d93484d8154ec4f9a17
PanferovDev <panferovdev@gmail.com>
2023-05-27 11:11:29 +0300

    client

--- server/server.js
+++ server/server.js
@@ -1,7 +1,14 @@
 const express = require("express");
+const http = require("http");
 
 const app = express();
+const port = 3000;
+
+const wsMap = new Map();
+
+app.use(express.static("public"));
+
 
 app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+  console.log("Server is running on port", port);
 });
